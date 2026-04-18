444a2b41c719b3b70f0dec5cc8b28bec661deb93
nextgencat <cavabanga027@gmail.com>
2026-04-18 21:49:05 +0200

    web upload

---
+++ src/script.js
@@ -0,0 +1,10 @@
+function scrollToProducts() {
+    const element = document.getElementById("products");
+    if (element) {
+        element.scrollIntoView({ behavior: "smooth" });
+    }
+}
+
+function joinTg() {
+    window.open("https://t.me/get_solana", '_blank')
+}
